95859286316de67d228d1b9d11de96dda0c87311
sct <sctsnipe@gmail.com>
2013-01-05 19:07:43 -0500

    Final commit for splash.php

--- splash.php
+++ splash.php
@@ -2,16 +2,16 @@
 
 header('Content-Type: image/png');
 
-$img = getcwd()."launcher/splash/1.png";
+$img = getcwd()."/launcher/splash/1.png";
 
 $im = @imagecreatefrompng($img);
 
 if(!$im)
     {
-        $im  = imagecreatetruecolor(150, 30);
+        $im  = imagecreatetruecolor(600, 30);
         $bgc = imagecolorallocate($im, 255, 255, 255);
         $tc  = imagecolorallocate($im, 0, 0, 0);
-        imagefilledrectangle($im, 0, 0, 150, 30, $bgc);
+        imagefilledrectangle($im, 0, 0, 600, 30, $bgc);
         imagestring($im, 1, 5, 5, 'Error loading ' . $img, $tc);
     }
 
