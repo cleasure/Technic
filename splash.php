4642791a47bfdf7359087743d98eeb339280f498
sct <sctsnipe@gmail.com>
2013-01-05 19:01:01 -0500

    Splash.php, why the hate

--- splash.php
+++ splash.php
@@ -1,10 +1,20 @@
 <?php
 
+header('Content-Type: image/png');
+
 $img = "launcher/splash/1.png";
 
 $im = @imagecreatefrompng($img);
 
-header('Content-Type: image/png');
+if(!$im)
+    {
+        $im  = imagecreatetruecolor(150, 30);
+        $bgc = imagecolorallocate($im, 255, 255, 255);
+        $tc  = imagecolorallocate($im, 0, 0, 0);
+        imagefilledrectangle($im, 0, 0, 150, 30, $bgc);
+        imagestring($im, 1, 5, 5, 'Error loading ' . $imgname, $tc);
+    }
+
 
 imagepng($im);
 imagedestroy($im);
